59ff297143f229570ed174f9f7fa49e5e1ab4212
Joselita Judite <joselitajuditedesouza@gmail.com>
2025-02-27 00:09:47 -0300

    adicionar modal na finalização da compra

--- meu-carrinho/carrinho.js
+++ meu-carrinho/carrinho.js
@@ -1,6 +1,10 @@
 const carrinho = {};
 let desconto = 0;
+let SUB_TOTAL = 0;
+let VALOR_DESCONTO = 0
+let TOTAL = 0;
 const FRETE_FIXO = 15.0;
+let MENSAGEM_COMPRA_FINALIZADA = "";
 
 function atualizarProduto(produto, operacao) {
   const id = produto.dataset.id;
@@ -29,12 +33,87 @@ function atualizarProduto(produto, operacao) {
 function atualizarResumo() {
     const subtotal = Object.values(carrinho).reduce((acc, item) => acc + item.quantidade * item.preco, 0);
     const valorDesconto = subtotal * desconto;
-    const total = subtotal - valorDesconto + FRETE_FIXO;
+    const frete = subtotal > 99.99 ? 0 : FRETE_FIXO;
+    const total = subtotal - valorDesconto + frete;
 
+  
     document.getElementById("subtotal").textContent = `R$ ${subtotal.toFixed(2)}`;
     document.getElementById("desconto").textContent = `- R$ ${valorDesconto.toFixed(2)}`;
+    document.getElementById("frete").textContent = `R$ ${frete.toFixed(2)}`;
     document.getElementById("total").textContent = `R$ ${total.toFixed(2)}`;
+
+    SUB_TOTAL = subtotal;
+    VALOR_DESCONTO = valorDesconto;
+    TOTAL = total;
+    MENSAGEM_COMPRA_FINALIZADA = criarMensagemCompraFinalizada(subtotal, valorDesconto, frete, total)
 }
+
+function criarMensagemCompraFinalizada(subtotal, valorDesconto, frete, total) {
+  const mensagem = `
+  Parabéns! Sua compra foi realizada com sucesso.
+
+  Detalhes do pedido:
+  - Subtotal: R$ ${subtotal.toFixed(2)}
+  - Desconto: R$ ${valorDesconto.toFixed(2)}
+  - Frete: R$ ${frete.toFixed(2)}
+  - Total: R$ ${total.toFixed(2)}
+
+  Obrigado por comprar conosco!
+  `;
+
+  return mensagem;
+}
+
+function exibirMensagem(mensagem) {
+  document.getElementById("comprar").addEventListener("click", (e) => {
+    alert(mensagem);
+    return;
+  })
+}
+
+function carrinhoVazio() {
+  if (SUB_TOTAL === 0) {
+    return true;
+  } 
+}
+
+function exibirModal(mensagem) {
+  let mensagemCarrinhoVazio = "";
+
+  if (carrinhoVazio()) {
+    mensagem = `Seu carrinho está vazio. 
+    
+                Adicione produtos antes de finalizar a compra.`;
+    mensagemCarrinhoVazio = "Carrinho vazio :(";
+  }
+
+  document.getElementById("titulo-modal").innerText = mensagemCarrinhoVazio;
+  document.getElementById("detalhes-compra").innerText = mensagem;
+  document.getElementById("modal").classList.add("active");
+}
+
+function fecharModal() {
+  document.getElementById("modal").classList.remove("active");
+}
+
+function resetarCampos() {
+  desconto = 0;
+  SUB_TOTAL = 0;
+  VALOR_DESCONTO = 0;
+  TOTAL = 0;
+  MENSAGEM_COMPRA_FINALIZADA = "";
+}
+
+function comprar() {
+  exibirModal(MENSAGEM_COMPRA_FINALIZADA);
+  resetarCampos();
+}
+
+document.getElementById("comprar").addEventListener("click", () => {
+   comprar();
+   return;
+});
+
 
 document.getElementById("produtos").addEventListener("click", (e) => {
   if (e.target.classList.contains("add")) {
@@ -60,11 +139,11 @@ document.getElementById("cupom").addEventListener("blur", (e) => {
   atualizarResumo();
 });
 
-document.getElementById("limpar").addEventListener("click", () => {
-    for (const id in carrinho) {
-      carrinho[id].quantidade = 0;
-    }
-    document.querySelectorAll(".quantidade").forEach((el) => (el.textContent = "0"));
-    document.querySelectorAll(".valor-produto").forEach((el) => (el.textContent = "R$ 0.00"));
-    atualizarResumo();
-  });
+// document.getElementById("limpar").addEventListener("click", () => {
+//     for (const id in carrinho) {
+//       carrinho[id].quantidade = 0;
+//     }
+//     document.querySelectorAll(".quantidade").forEach((el) => (el.textContent = "0"));
+//     document.querySelectorAll(".valor-produto").forEach((el) => (el.textContent = "R$ 0.00"));
+//     atualizarResumo();   
+// }); 
